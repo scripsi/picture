dbc84edec7440d211c34f702e71f41568e66e228
Scripsi <scripsi@scripsi.uk>
2021-05-10 21:39:34 +0100

    long exposure

--- picture.py
+++ picture.py
@@ -1,9 +1,16 @@
 from picamera import PiCamera
 from time import sleep
+from fractions import Fraction
 
-camera = PiCamera()
+camera = PiCamera(
+    resolution=(1280, 720),
+    framerate=Fraction(1, 6),
+    sensor_mode=3)
+camera.shutter_speed = 6000000
+camera.iso = 800
 
+camera.exposure_mode = 'off'
 camera.start_preview()
-sleep(5)
+sleep(30)
 camera.capture('/home/pi/picture/image.jpg')
 camera.stop_preview()
