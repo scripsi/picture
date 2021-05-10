3b15d86efea8244a19f1c64efeedc031c68f9dc8
Scripsi <scripsi@scripsi.uk>
2021-05-10 22:14:43 +0100

    added leds

--- picture.py
+++ picture.py
@@ -1,7 +1,12 @@
+from gpiozero import LED
 from picamera import PiCamera
 from time import sleep
 from fractions import Fraction
 
+led1 = LED(23)
+led2 = LED(24)
+led1.on()
+led2.on()
 camera = PiCamera(
     resolution=(1280, 720),
     framerate=Fraction(1, 6),
@@ -14,3 +19,5 @@ camera.start_preview()
 sleep(30)
 camera.capture('/home/pi/picture/image.jpg')
 camera.stop_preview()
+led1.off()
+led2.off()
